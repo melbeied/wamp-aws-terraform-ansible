0da0637431b37313b9831285ac51be86f905503f
melbeied <melbeied@gmail.com>
2020-06-29 17:53:21 +0100

    update with resolvin isues

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,17 +1,12 @@
 variable "ami_id" {
-  default = "ami-bfff49c2"
+  default = "ami-0c24eddbea3a65909"
 }
-// region = eu-west-3 : 
-// source https://wiki.centos.org/Cloud/AWS
-// https://aws.amazon.com/marketplace/pp/Centosorg-CentOS-7-x8664-with-Updates-HVM/B00O7WM7QW#pdp-pricing
 
 variable "instance_bastion_type" {
   default   = "t2.micro"
-  #default   = "t2.medium"
 }
 
 variable "instance_wp_type" {
-  #default   = "t2.micro"
   default   = "t2.medium"
 }
 
@@ -20,5 +15,5 @@ variable "public_key_name" {
 }
 
 variable "public_key_path" {
-  default   = "/workspace/vagrant/aws/workspace/keys"
+  default   = "/workspace/vagrant/aws/workspace/wamp-aws-terraform-ansible/keys"
 }
